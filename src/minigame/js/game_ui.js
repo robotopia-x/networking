0bce89725ec88090e1bf6142d815f395503b6834
Johannes Kahlhöfer <johannes.kahlhoefer@exxeta.com>
2016-11-10 14:06:55 +0100

    works

--- src/minigame/js/game_ui.js
+++ src/minigame/js/game_ui.js
@@ -9,32 +9,33 @@ var game_ui = {};
   game_ui.canvas = null
   game_ui.game = null
   game_ui.tileSize = null
+  game_ui.gridsize = {}
 
   function drawGrid () {
-    var fullWidth, fullHeight, gridSize, x, y, context, grid
+    var fullWidth, fullHeight, x, y, context, grid
     context = game_ui.canvas.getContext('2d')
     grid = game_ui.game.grid
-    gridSize = {
+    game_ui.gridsize = {
       x: grid[ 0 ].length,
       y: grid.length
     }
     fullWidth = context.canvas.width
     fullHeight = context.canvas.height
     game_ui.tileSize = {
-      width: Math.round(fullWidth / gridSize.x),
-      height: Math.round(fullHeight / gridSize.y)
+      width: Math.round(fullWidth / game_ui.gridsize.x),
+      height: Math.round(fullHeight / game_ui.gridsize.y)
     }
     context.clearRect(0, 0, fullWidth, fullHeight)
 
-    for (x = 0; x < gridSize.x; x++) {
-      for (y = 0; y < gridSize.y; y++) {
-        drawTile({ x: x * tileSize.width, y: y * tileSize.height }, grid[ y ][ x ])
+    for (x = 0; x < game_ui.gridsize.x; x++) {
+      for (y = 0; y < game_ui.gridsize.y; y++) {
+        drawTile({ x: x * game_ui.tileSize.width, y: y * game_ui.tileSize.height }, grid[ y ][ x ])
       }
     }
 
     function drawTile (position, tile) {
       context.fillStyle = tile.backgroundColor.background
-      context.fillRect(position.x, position.y, tileSize.width, tileSize.height)
+      context.fillRect(position.x, position.y, game_ui.tileSize.width, game_ui.tileSize.height)
       context.font = '20px Arial'
       context.fillStyle = tile.fontColor.font
       context.fillText(tile.text.name, position.x + 30, position.y + 30)
@@ -48,11 +49,23 @@ var game_ui = {};
   function registerCanvas (canvas) {
     game_ui.canvas = canvas
     game_ui.canvas.onclick = function (event) {
+      if (!game_ui.game) {
+        return
+      }
       var x, y
       console.log('Clicked: ' + event.offsetX + ', ' + event.offsetY)
-      x = event.offsetX
-      y = event.offsetY
+      var hitboxForTiles = {
+        x: canvas.getContext('2d').canvas.clientWidth / game_ui.gridsize.x,
+        y: canvas.getContext('2d').canvas.clientHeight / game_ui.gridsize.y
+      }
+      x = Math.floor(event.offsetX / hitboxForTiles.x)
+      y = Math.floor(event.offsetY / hitboxForTiles.y)
+      console.log('Clicked - in tiles: ' + x + ', ' + y)
       // TODO: check for rect/tile, if is right.
+      if (game_ui.game.correct.x === x && game_ui.game.correct.y === y) {
+        console.log("correct!")
+        restartGame()
+      }
     }
   }
 
@@ -61,4 +74,6 @@ var game_ui = {};
     game_ui.span.innerHTML = game_ui.game.task
     drawGrid()
   }
+
+
 })()
