f5fbbabe8684e8c4b6461207c81c54d76f4d6745
Johannes Kahlhöfer <johannes.kahlhoefer@exxeta.com>
2016-12-20 21:50:04 +0100

    closing respective swarm upon setup, logging mode

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var hub = 'http://localhost:8042'
 
 window.startMain = function () {
   console.log('starting a main')
-  var peerMain = new ps({isMain: true, GID: 'Group', hubURL: hub})
+  var peerMain = new ps({isMain: true, GID: 'Group', hubURL: hub, log: true})
 }
 
 window.startClient = function () {
   console.log('starting a client')
-  var peerClient = new ps({isMain: false, GID: 'Group', hubURL: hub})
+  var peerClient = new ps({isMain: false, GID: 'Group', hubURL: hub, log: true})
 }
