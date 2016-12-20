400440860e6854656f41bba3c9a2dd084b3b203f
Johannes Kahlhöfer <johannes.kahlhoefer@exxeta.com>
2016-12-20 21:59:03 +0100

    standard run

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var ps = require('./lib/index.js')
+var Ps = require('./lib/index.js')
 var hub = 'http://localhost:8042'
-//var hub = 'signalhub.perguth.de'
+// var hub = 'signalhub.perguth.de'
 
 window.startMain = function () {
   console.log('starting a main')
-  var peerMain = new ps({isMain: true, GID: 'Group', hubURL: hub, log: true})
+  Ps({isMain: true, GID: 'Group', hubURL: hub, log: true})
 }
 
 window.startClient = function () {
   console.log('starting a client')
-  var peerClient = new ps({isMain: false, GID: 'Group', hubURL: hub, log: true})
+  Ps({isMain: false, GID: 'Group', hubURL: hub, log: true})
 }
