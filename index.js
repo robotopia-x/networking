74b34c11436b8341cd4a67e98373441a5ba6af1d
Johannes Kahlhöfer <johannes.kahlhoefer@exxeta.com>
2016-12-29 18:07:27 +0100

    signaling not working out, package.json most probably inaccurate state now

--- index.js
+++ index.js
@@ -2,46 +2,14 @@ var Ps = require('./lib/index.js')
 var hub = 'http://localhost:8042'
 // var hub = 'signalhub.perguth.de'
 
+localStorage.debug = "peer-star"
+
 window.startMain = function () {
   console.log('starting a main')
-  var logger = createHTMLAppender('log')
-  Ps({isMain: true, GID: 'Group', hubURL: hub, logger: logger})
+  new Ps({isMain: true, GID: 'Group', hubURL: hub})
 }
 
 window.startClient = function () {
   console.log('starting a client')
-  var logger = createHTMLAppender('log')
-  Ps({isMain: false, GID: 'Group', hubURL: hub, logger: logger})
-}
-
-function createHTMLAppender(id) {
-  const max = 20
-  var appender, list
-  try {
-    document.getElementById(id).innerHTML = 'will log here'
-  } catch (err) {
-    throw 'Error creating Appender'
-  }
-
-  list = []
-  appender = function (m) {
-    console.log(m)
-    var log, i
-    list.push(m)
-    if (list.length > max) {
-      list = list.slice( list.length - max )
-    }
-    log = ''
-    for (i in list) {
-      log += list[i] + '<br>'
-    }
-    try {
-      document.getElementById(id).innerHTML = log
-    } catch (err) {
-      throw 'Error appending'
-    }
-  }
-
-  return appender
-
+  new Ps({isMain: false, GID: 'Group', hubURL: hub})
 }
