b61883fe29e7a32c43f276e61fba1aee2c038b7c
Johannes Kahlhöfer <johannes.kahlhoefer@exxeta.com>
2016-12-29 14:57:06 +0100

    added logging to innerhtml

--- index.js
+++ index.js
@@ -4,10 +4,44 @@ var hub = 'http://localhost:8042'
 
 window.startMain = function () {
   console.log('starting a main')
-  Ps({isMain: true, GID: 'Group', hubURL: hub, log: true})
+  var logger = createHTMLAppender('log')
+  Ps({isMain: true, GID: 'Group', hubURL: hub, logger: logger})
 }
 
 window.startClient = function () {
   console.log('starting a client')
-  Ps({isMain: false, GID: 'Group', hubURL: hub, log: true})
+  var logger = createHTMLAppender('log')
+  Ps({isMain: false, GID: 'Group', hubURL: hub, logger: logger})
+}
+
+function createHTMLAppender(id) {
+  const max = 20
+  var appender, list
+  try {
+    document.getElementById(id).innerHTML = 'will log here'
+  } catch (err) {
+    throw 'Error creating Appender'
+  }
+
+  list = []
+  appender = function (m) {
+    console.log(m)
+    var log, i
+    list.push(m)
+    if (list.length > max) {
+      list = list.slice( list.length - max )
+    }
+    log = ''
+    for (i in list) {
+      log += list[i] + '<br>'
+    }
+    try {
+      document.getElementById(id).innerHTML = log
+    } catch (err) {
+      throw 'Error appending'
+    }
+  }
+
+  return appender
+
 }
