252f1e54c512ae152c10ae5b3ae442c808ecfbce
Johannes Kahlhöfer <johannes.kahlhoefer@exxeta.com>
2016-12-30 14:01:20 +0100

    short useage change. 'new' moved to framework

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var Ps = require('./lib/index.js')
+var ps = require('./lib/index.js')
 var hub = 'http://localhost:8042'
 // var hub = 'signalhub.perguth.de'
 
@@ -6,10 +6,10 @@ localStorage.debug = "peer-star"
 
 window.startMain = function () {
   console.log('starting a main')
-  new Ps({isMain: true, GID: 'Group', hubURL: hub})
+  ps({isMain: true, GID: 'Group', hubURL: hub})
 }
 
 window.startClient = function () {
   console.log('starting a client')
-  new Ps({isMain: false, GID: 'Group', hubURL: hub})
+  ps({isMain: false, GID: 'Group', hubURL: hub})
 }
