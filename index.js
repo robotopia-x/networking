f31df21e3b73d392c177c6fc9ed901089e8253de
Johannes Kahlhöfer <johannes.kahlhoefer@exxeta.com>
2016-12-30 15:23:22 +0100

    added close functionality

--- index.js
+++ index.js
@@ -1,35 +1,55 @@
 var ps = require('./lib/index.js')
 var hub = 'http://localhost:8042'
-// var hub = 'signalhub.perguth.de'
+var stars = {}
 
 localStorage.debug = "peer-star"
 
-window.startMain = function () {
-  console.log('starting a main')
-  nodes.add([{id: 'ME', label: 'ME'}])
-  var p = ps({isMain: true, GID: 'Group', hubURL: hub})
-  p.on('connect', function (peer, id) {
-    nodes.add([{id: id, label: id}])
-    edges.add([{from: id, to: 'ME'}])
-  })
-  p.on('disconnect', function (peer, id) {
-    nodes.remove(id)
-    edges.remove(id)
-  })
+window.buttonMain = function () {
+  if (!stars.main) {
+    document.getElementById('main').innerHTML = 'Stop Main'
+    console.log('starting a main')
+    nodes.add([{id: 'ME', label: 'ME'}])
+    var p = stars.main = ps({isMain: true, GID: 'Group', hubURL: hub})
+    p.on('connect', function (peer, id) {
+      nodes.add([{id: id, label: id}])
+      edges.add([{from: id, to: 'ME'}])
+    })
+    p.on('disconnect', function (peer, id) {
+      nodes.remove(id)
+      edges.remove(id)
+    })
+  } else {
+    stars.main.close(function () {
+      delete stars.main
+      edges.clear()
+      nodes.clear()
+      document.getElementById('main').innerHTML = 'Start Main'
+    })
+  }
 }
 
-window.startClient = function () {
-  console.log('starting a client')
-  nodes.add([{id: 'ME', label: 'ME'}])
-  var p = ps({isMain: false, GID: 'Group', hubURL: hub})
-  p.on('connect', function (peer, id) {
-    nodes.add([{id: id, label: id}])
-    edges.add([{from: id, to: 'ME'}])
-  })
-  p.on('disconnect', function (peer, id) {
-    nodes.remove(id)
-    edges.remove(id)
-  })
+window.buttonClient = function () {
+  if (!stars.client) {
+    document.getElementById('client').innerHTML = 'Stop Client'
+    console.log('starting a client')
+    nodes.add([{id: 'ME', label: 'ME'}])
+    var p = stars.client = ps({isMain: false, GID: 'Group', hubURL: hub})
+    p.on('connect', function (peer, id) {
+      nodes.add([{id: id, label: id}])
+      edges.add([{from: id, to: 'ME'}])
+    })
+    p.on('disconnect', function (peer, id) {
+      nodes.remove(id)
+      edges.remove(id)
+    })
+  } else {
+    stars.client.close(function () {
+      delete stars.client
+      edges.clear()
+      nodes.clear()
+      document.getElementById('client').innerHTML = 'Start Client'
+    })
+  }
 }
 
 var nodes = new vis.DataSet({});
